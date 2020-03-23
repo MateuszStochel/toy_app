43d2732aeebaa1b58ef940b0ced3fd8445e1a4c6
matteuusz Stochel <mateuszstochel1995@gmail.com>
2020-03-23 23:27:27 +0100

    Add delete funcionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,7 +21,24 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+  def update
+    @post = Post.find(params[:id])
 
+    if @post.update(post_params) 
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+  def edit
+    @post = Post.find(params[:id])
+  end
   private
 
   def post_params
